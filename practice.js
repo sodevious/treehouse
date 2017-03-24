86b8a9056ce16db2628d92f96b03587836960358
Nicole Dominguez <work@nicoledominguez.com>
2017-03-24 13:41:26 -0600

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,12 @@
+
+function practice() {
+  
+  var first = prompt("What's your first name?");
+  var last = prompt("What's your last name?");
+  
+  var name = first + " " + last;
+
+  alert("The string " + name.toUpperCase() + " is " + name.length + " number of chararacters long.")
+};
+
+practice();
